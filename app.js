03411fe1969c6d913b53f16fbb9adde9e2f1d33d
TeoKar <38850740+TeoKar@users.noreply.github.com>
2019-03-12 21:10:02 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const request = require('request');
+const fetch = require("node-fetch");
+
+
+const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+setInterval(function(){
+const req = {
+    url: 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',
+    method: 'POST',
+    headers: {"Content-Type": "application/json"},
+    body: {
+        "query": `{
+    stop(id: \"HSL:4150201\") {
+      name
+      stoptimesWithoutPatterns {
+      trip {
+        route {
+          shortName
+          longName
+        }
+      }
+      scheduledArrival
+      realtimeArrival
+      arrivalDelay
+      scheduledDeparture
+      realtimeDeparture
+      departureDelay
+      realtime
+      realtimeState
+      }
+      
+      wheelchairBoarding  }
+  }`
+    },
+    json: true
+};
+
+request(req, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+        console.log(JSON.stringify(body, null, 4));
+        app.use('/l',(req, res) => {
+            res.send(JSON.stringify(body, null, 4))
+        });
+    }
+});
+},  4990);
+
+
+fetch('https://www.sodexo.fi/ruokalistat/output/daily_json/16365/2019/03/13/fi')
+    .then(res => res.json())
+    .then(json => app.use('/i',(req, res) => {
+        res.send(json);
+        console.log(json)
+    })
+);
+
+
+app.listen(8002, () => {
+    console.log('Listening on port 8002');
+});
---
+++ public/js/front.js
@@ -0,0 +1,114 @@
+'use strict';
+const sodexo = document.querySelector('#sodexo');
+const node = document.querySelector('#hsl');
+const body = document.querySelector("body");
+
+
+fetch('/l').then(function (response) {
+    return response.json();
+}).then(function (json) {
+    hslFu(json);
+});
+
+function hslFu(hslLista) {
+    console.log(hslLista);
+    for (let y = 0; y < 5;) {
+        const pysakki = hslLista.data.stop.name;
+        const saapuminen = hslLista.data.stop.stoptimesWithoutPatterns[y].realtimeArrival;
+        const lahto = hslLista.data.stop.stoptimesWithoutPatterns[y].realtimeDeparture;
+        const reitti = hslLista.data.stop.stoptimesWithoutPatterns[y].trip.route.longName;
+        const lyhytNimi = hslLista.data.stop.stoptimesWithoutPatterns[y].trip.route.shortName;
+        const oikeaAika = hslLista.data.stop.stoptimesWithoutPatterns[y].realtimeState;
+        const d = document.createElement('div');
+        d.classList.add("HSL");
+        d.innerHTML = pysakki + " " + reitti + " " + lyhytNimi + " " + saapuminen + " " + lahto + " " + oikeaAika;
+
+        hsl.appendChild(d);
+        y++;
+    }
+}
+
+fetch('/i').then(function (response) {
+    return response.json();
+}).then(function (json) {
+    naytaLista(json);
+});
+
+function naytaLista(sode) {
+    JSON.stringify(sode);
+    console.log(sode);
+    for (let x = 0; x < 8;) {
+        const hinta = sode.courses[x].price;
+        const nimi = sode.courses[x].title_fi;
+        const prop = sode.courses[x].properties;
+
+        const d = document.createElement('div');
+        d.classList.add("sodexo");
+        d.innerHTML = hinta + nimi + prop;
+
+        sodexo.appendChild(d);
+        x++;
+    }
+}
+
+let timer1 = setInterval(() =>
+    fetch('/l').then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        hslFu1(json);
+
+function hslFu1(hslLista) {
+    try {
+        node.innerHTML = "";
+        console.log("clear");
+    } catch (err) {
+        console.log(err);
+    }
+    console.log(hslLista);
+    for (let y = 0; y < 5;) {
+        const pysakki = hslLista.data.stop.name;
+        const saapuminen = hslLista.data.stop.stoptimesWithoutPatterns[y].realtimeArrival;
+        const lahto = hslLista.data.stop.stoptimesWithoutPatterns[y].realtimeDeparture;
+        const reitti = hslLista.data.stop.stoptimesWithoutPatterns[y].trip.route.longName;
+        const lyhytNimi = hslLista.data.stop.stoptimesWithoutPatterns[y].trip.route.shortName;
+        const oikeaAika = hslLista.data.stop.stoptimesWithoutPatterns[y].realtimeState;
+
+        const d = document.createElement('div');
+        d.classList.add("HSL");
+        d.innerHTML = pysakki + " " + reitti + " " + lyhytNimi + " " + saapuminen + " " + lahto + " " + oikeaAika;
+
+        hsl.appendChild(d);
+        y++;
+    }
+}
+}), 10000);
+
+
+let timer2 = setInterval(() =>
+    fetch('/i').then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        naytaLista1(json);
+
+        function naytaLista1(sode) {
+            JSON.stringify(sode);
+            try {
+                sodexo.innerHTML = "";
+            } catch (err) {
+                console.log(err);
+            }
+            console.log(sode);
+            for (let x = 0; x < 8;) {
+                const hinta = sode.courses[x].price;
+                const nimi = sode.courses[x].title_fi;
+                const prop = sode.courses[x].properties;
+
+                const d = document.createElement('div');
+                d.classList.add("sodexo");
+                d.innerHTML = hinta + nimi + prop;
+
+                sodexo.appendChild(d);
+                x++;
+            }
+        }
+    }), 600000);
